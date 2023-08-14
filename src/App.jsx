9cefcf84b793a321f24e07384937c8f177ba191b
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-08-14 11:30:00 +0200

    Livret opgaven

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import './App.css'
 import { HomePage } from './components/HomePage/HomePage.jsx'
+import { Livret } from './components/LivretPage/LivretPage.jsx'
 
 function App() {
   const nameArray = [{name:'kamilla'}, {name:'amina'}, {name:'cæcilie'}]
-  
+  const livretArray = [{name:'kylling'}, {name:'ananas'}, {name:'and'}]
   return (
     <>
-    {nameArray.map((item, index) =>{
+    <h2>Her navne-opgaven:</h2>
+    {nameArray.map((item, index) => {
       return(
       <HomePage key={index} name={item.name} />
     )
+      })}
+      <br />
+
+      {/* HER ER LIVRET OPGAVEN */}
+      <h2>Her er livret-opgaven:</h2>
+      {livretArray.map((item, index) => {
+        return(
+          <Livret key={index} name={item.name} />
+        )
       })}
   </>
   )
 }
 
-
 export default App
-
-
-
-/* Et Komponent skrevet i .jsx der tager imod props
-At komponentet er et child af App.jsx og renderes i browseren */
---
+++ src/components/LivretPage/LivretPage.jsx
@@ -0,0 +1,6 @@
+export const Livret = (props) => {
+    
+    return (
+            <h2>{props.name}</h2>
+    )
+}
